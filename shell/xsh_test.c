6941a18bb3e05a072e00499d24d37b0bffe91ca4
Héctor Reyes <139267254+HARRVL@users.noreply.github.com>
2024-04-22 17:40:43 -0500

    Update xsh_test.c

--- shell/xsh_test.c
+++ shell/xsh_test.c
@@ -12,9 +12,60 @@
  * @param args array of arguments
  * @return OK for success, SYSERR for syntax error
  */
+// command xsh_test(int nargs, char *args[])
+// {
+//    TODO: Test your O/S.
+//    printf("This is where you should put some testing code.\n");
+//     return OK;
+// }
+
 command xsh_test(int nargs, char *args[])
 {
-    //TODO: Test your O/S.
-    printf("This is where you should put some testing code.\n");
+    // Create a file
+    int fd = fileCreate("testfile");
+    if (fd == SYSERR)
+    {
+        printf("Test failed: file creation failed.\n");
+        return SYSERR;
+    }
+    else
+    {
+        printf("File created successfully.\n");
+    }
+
+    // Write data to the file
+    char data = 'X';
+    if (filePutChar(fd, data) == SYSERR)
+    {
+        printf("Test failed: writing to file failed.\n");
+        return SYSERR;
+    }
+    else
+    {
+        printf("Data written to file successfully.\n");
+    }
+
+    // Close the file
+    if (fileClose(fd) == SYSERR)
+    {
+        printf("Test failed: file closing failed.\n");
+        return SYSERR;
+    }
+    else
+    {
+        printf("File closed successfully.\n");
+    }
+
+    // Delete the file
+    if (fileDelete(fd) == SYSERR)
+    {
+        printf("Test failed: file deletion failed.\n");
+        return SYSERR;
+    }
+    else
+    {
+        printf("File deleted successfully.\n");
+    }
+
     return OK;
 }
