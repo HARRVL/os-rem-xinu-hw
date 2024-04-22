d7513f4d5c8b2aacd3a4019b066b649be21b4f9d
Héctor Reyes <139267254+HARRVL@users.noreply.github.com>
2024-04-22 17:24:38 -0500

    Update fileDelete.c

--- file/fileDelete.c
+++ file/fileDelete.c
@@ -17,9 +17,63 @@
  */
 devcall fileDelete(int fd)
 {
-    // TODO: Unlink this file from the master directory index,
-    //  and return its space to the free disk block list.
-    //  Use the superblock's locks to guarantee mutually exclusive
-    //  access to the directory index.
+    struct dentry *phw;
+    int devtab;
+
+    // Error check the file descriptor (fd)
+    if (isbadfd(fd))
+    {
+        return SYSERR;
+    }
+
+    // Error check superblock
+    if (NULL == supertab)
+    {
+        return SYSERR;
+    }
+
+    // Lock the directory block for mutual exclusion
+    wait(supertab->sb_dirlock);
+
+    // Error check the directory list
+    if (NULL == supertab->sb_dirlst)
+    {
+        signal(supertab->sb_dirlock);
+        return SYSERR;
+    }
+
+    // Get a pointer to the file's directory entry
+    struct filenode *fnode = &supertab->sb_dirlst->db_fnodes[fd];
+
+    // Error check file state
+    if (FILE_USED != fnode->fn_state)
+    {
+        signal(supertab->sb_dirlock);
+        return SYSERR;
+    }
+
+    // Reset values of the file (name, state, length, etc.)
+    bzero(fnode->fn_name, FNAMLEN + 1);
+    fnode->fn_state = FILE_FREE;
+    fnode->fn_length = 0;
+    fnode->fn_cursor = 0;
+
+    // Remove data from hard drive with sbFreeBlock and error check
+    if (SYSERR == sbFreeBlock(supertab, fnode->fn_blocknum))
+    {
+        signal(supertab->sb_dirlock);
+        return SYSERR;
+    }
+
+    // Update hard drive info with seek and write
+    phw = supertab->sb_disk->dvnum;
+    devtab = ((struct disk *)devtab_get(phw))->disk_blocknum;
+
+    seek(devtab, supertab->sb_blocknum);
+    write(devtab, supertab, sizeof(struct superblock));
+
+    // Signal semaphore to end mutual exclusion
+    signal(supertab->sb_dirlock);
+
     return OK;
 }
