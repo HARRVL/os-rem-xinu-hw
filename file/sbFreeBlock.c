0998c2c33594e4689db83ca5b8306a5e24914bc1
Héctor Reyes <139267254+HARRVL@users.noreply.github.com>
2024-04-22 17:25:52 -0500

    Update sbFreeBlock.c

--- file/sbFreeBlock.c
+++ file/sbFreeBlock.c
@@ -18,10 +18,55 @@
  *------------------------------------------------------------------------
  */
 devcall sbFreeBlock(struct superblock *psuper, int block)
-{
+
     // TODO: Add the block back into the filesystem's list of
     //  free blocks.  Use the superblock's locks to guarantee
     //  mutually exclusive access to the free list, and write
     //  the changed free list segment(s) back to disk.
-    return SYSERR;
+{
+    struct freeblock *freeblk;
+    struct freeblock *collector;
+    int devtab;
+
+    // Error check if superblock is null
+    if (NULL == psuper)
+    {
+        return SYSERR;
+    }
+
+    // Error check block number
+    if ((block < 0) || (block >= psuper->sb_blocktotal))
+    {
+        return SYSERR;
+    }
+
+    // Lock free list for mutual exclusion
+    wait(psuper->sb_freelock);
+
+    // Get the first collector node
+    collector = psuper->sb_freelst;
+
+    // Case: if the disk is completely full
+    if (0 == collector->fr_count)
+    {
+        // Create a new collector node
+        // (Note: Actual implementation of adding collector nodes is needed)
+    }
+    else
+    {
+        // Add to the next available index in collector node
+        collector->fr_free[collector->fr_count++] = block;
+    }
+
+    // Write this information to the disk
+    devtab = ((struct disk *)devtab_get(psuper->sb_disk->dvnum))->disk_blocknum;
+
+    seek(devtab, collector->fr_blocknum);
+    write(devtab, collector, sizeof(struct freeblock));
+
+    // Signal semaphore to end mutual exclusion
+    signal(psuper->sb_freelock);
+
+    return OK;
 }
+
