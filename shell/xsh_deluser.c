2f288f4d6b37e9cfc9a970302701355f9ed78229
maxpenaaa <158525268+maxpenaaa@users.noreply.github.com>
2024-05-01 22:02:46 -0500

    Update xsh_deluser.c

--- shell/xsh_deluser.c
+++ shell/xsh_deluser.c
@@ -1,1 +1,43 @@
 
+/**
+ * @file     xsh_switchuser.c
+ * @provides xsh_switchuser
+ *
+ */
+/* Embedded XINU, Copyright (C) 2009.  All rights reserved. */
+
+#include <xinu.h>
+
+/**
+ * Shell command (delUSer) is deletinguser hook.
+ * @param args array of arguments
+ * @return OK for success, SYSERR for syntax error
+ */
+command xsh_deluser(int nargs, char *args[])
+{
+    int id;
+
+    if (userid < SUPERUID)
+    {
+        printf("Must login first\n");
+        return SYSERR;
+    }
+
+    //Attempt authentication
+    if (nargs == 2)
+    {
+        id = login(args[1]);
+    }
+    else
+    {
+        id = login(NULL);
+    }
+    if (SYSERR == id)
+    {
+        printf("Login failure.\n");
+        return SYSERR;
+    }
+    userid = id;
+    printf("Success!\n");
+    return OK;
+}
